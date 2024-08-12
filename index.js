99e800e323ba935446498cb49b1abe37807da0a3
077tobi <149345589+077tobi@users.noreply.github.com>
2024-08-12 10:50:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// Use o body-parser para analisar o corpo das requisições
+app.use(bodyParser.json());
+
+// Dados dos novos episódios (substitua pelo seu JSON real)
+let novosEpisodios = [
+  {
+    "response": "success",
+    "video_id": "498604",
+    "category_id": "112",
+    "title": "One Piece Episodio 1121",
+    "location": "https://get.atv2.net/m.php?token=ZXdkUWJma2tQOW5uOVhnTUJ3RkFWcnZhU3ZCbkgrQzFCYTFYU3BsT2h5TVcrK0hXZWFpRUFCNzhHYjVKNnYrc05OandMQXVqZTltSzBVOURNdEpYcnp5RytUVzRpd0NGWFM2WVJTN2NnV2k4QkVQTjhtakRqeUZUdU1KOGcrUURDdHJnRi9yWUNCT0pyQUE2VkFzUEFOMWlzTnFIeEVSMS9yZE00Ri82clZFUThJQzhQNkFmUXpsdDZTL3dhQ01V&qh=sd",
+    "sdlocation": "https://get.atv2.net/m.php?token=ZXdkUWJma2tQOW5uOVhnTUJ3RkFWcnZhU3ZCbkgrQzFCYTFYU3BsT2h5TVcrK0hXZWFpRUFCNzhHYjVKNnYrc05OandMQXVqZTltSzBVOURNdEpYcnp5RytUVzRpd0NGWFM2WVJTN2NnV2k4QkVQTjhtakRqeUZUdU1KOGcrUURDdHJnRi9yWUNCT0pyQUE2VkFzUEFOMWlzTnFIeEVSMS9yZE00Ri82clZFUThJQzhQNkFmUXpsdDZTL3dhQ01V&qh=hd",
+    "image": "7f6ffaa6bb0b408017b62254211691b5.jpg"
+  },
+  // ... (adicione os outros episódios aqui)
+];
+
+// Rota para obter a lista de novos episódios
+app.get('/episodios', (req, res) => {
+  res.json(novosEpisodios);
+});
+
+// Rota para enviar um novo episódio
+app.post('/episodios', (req, res) => {
+  const novoEpisodio = req.body;
+  novosEpisodios.push(novoEpisodio);
+  res.json({ message: 'Episódio adicionado com sucesso!' });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
